86c0a8afe5ce8d93ec3257fe883e6c05a7e522c3
Chaitanya <VankarChaitanya@outlook.com>
2020-10-02 22:50:09 +0530

    IndianFlag

--- Flag.py
+++ Flag.py
@@ -5,7 +5,7 @@ title("Indian Flag")
 penup()
 goto(-400, 250)
 pendown()
-
+# orange color
 begin_fill()
 
 color("Orange")
@@ -21,7 +21,7 @@ end_fill()
 left(90)
 forward(167)
 
-
+#green color
 color("Green")
 begin_fill()
 forward(167)
@@ -32,7 +32,7 @@ forward(167)
 end_fill()
 
 
-
+#main circle
 penup()
 goto(70,0)
 color("navy")
@@ -40,8 +40,7 @@ pendown()
 begin_fill()
 circle(70)
 end_fill()
-
-
+#white circle
 penup()
 goto(60,0)
 color("white")
@@ -49,12 +48,7 @@ pendown()
 begin_fill()
 circle(60)
 end_fill()
-
-
-
-
-
-
+#wheel like look
 penup()
 goto(-57, -8)
 pendown()
@@ -67,18 +61,14 @@ for i in range(24):
     forward(15)
     right(15)
     pendown()
-
-
+#inner circle
 penup()
 goto(20,0)
 pendown()
 begin_fill()
 circle(20)
 end_fill()
-
-
-
-
+#spikes of wheel
 penup()
 goto(0, 0)
 pendown()
@@ -86,8 +76,6 @@ for i in range(24):
     forward(60)
     backward(60)
     left(15)
-
-
 hideturtle()
 
 
