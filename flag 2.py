74423331c48b670d76a0621c54474259cba51921
Chaitanya <VankarChaitanya@outlook.com>
2020-10-04 17:50:27 +0530

    Olympics flag

---
+++ flag 2.py
@@ -0,0 +1,34 @@
+from turtle import *
+speed(0)
+setup(800, 500)
+title('Olympics Flag')
+
+pensize(14)
+
+
+pencolor("black")
+circle(80)
+
+penup()
+goto(-200, 0)
+pendown()
+pencolor("darkblue")
+circle(80)
+
+penup()
+goto(200, 0)
+pendown()
+pencolor("red")
+circle(80)
+
+penup()
+goto(-100, -88)
+pendown()
+pencolor("yellow")
+circle(80)
+
+penup()
+goto(100, -88)
+pendown()
+pencolor("green")
+circle(80)
