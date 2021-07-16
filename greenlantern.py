8dbbf4d56ac55a4bea7ac47739e23bc828f1016d
Chaitanya <vankar_chaitanya@yahoo.com>
2021-07-16 23:20:48 +0530

    Add files via upload

---
+++ greenlantern.py
@@ -0,0 +1,51 @@
+from turtle import *
+speed(0)
+setup(800,500)
+bgcolor('green')
+
+#typography
+pensize(16)
+
+#background cricle
+penup()
+goto(-50,-50)
+pendown()
+color("white")
+begin_fill()
+circle(100)
+end_fill()
+
+# #firstline
+# turtle.penup()
+# turtle.goto(0,10)
+# turtle.pendown()
+# turtle.pencolor("green")
+# turtle.right(-180)
+# turtle.forward(100)
+
+# #secondline
+# turtle.penup()
+# turtle.goto(-100,80)
+# turtle.pendown()
+# turtle.pencolor("green")
+# turtle.right(-180)
+# turtle.forward(100)
+
+# #middle circle
+# turtle.penup()
+# turtle.goto(-50,20)
+# turtle.pendown()
+# turtle.color("green")
+# turtle.begin_fill()
+# turtle.circle(24)
+# turtle.end_fill()
+
+# #small circle
+# turtle.penup()
+# turtle.goto(-50,35)
+# turtle.pendown()
+# turtle.color("white")
+# turtle.begin_fill()
+# turtle.circle(10)
+# turtle.end_fill()
+
